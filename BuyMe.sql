6ec997fef2997fd1fbf9cdfab8e4314b92e171e0
Abdul Uddin <65580428+auddin431@users.noreply.github.com>
2022-05-02 22:16:55 -0400

    Add files via upload

--- BuyMe.sql
+++ BuyMe.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `buyme` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `buyme`;
 -- MySQL dump 10.13  Distrib 8.0.28, for macos11 (x86_64)
 --
 -- Host: localhost    Database: buyme
@@ -16,6 +14,151 @@ USE `buyme`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alerts`
+--
+
+DROP TABLE IF EXISTS `alerts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alerts` (
+  `user_email` varchar(100) NOT NULL,
+  `type` enum('shirts','pants','shoes') NOT NULL,
+  PRIMARY KEY (`user_email`,`type`),
+  KEY `user_email_idx` (`user_email`),
+  CONSTRAINT `user_email` FOREIGN KEY (`user_email`) REFERENCES `user` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alerts`
+--
+
+LOCK TABLES `alerts` WRITE;
+/*!40000 ALTER TABLE `alerts` DISABLE KEYS */;
+INSERT INTO `alerts` VALUES ('user1@gmail.com','shoes');
+/*!40000 ALTER TABLE `alerts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `auction_posts`
+--
+
+DROP TABLE IF EXISTS `auction_posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auction_posts` (
+  `aID` int NOT NULL,
+  `close_time` time NOT NULL,
+  `close_date` datetime NOT NULL,
+  `increment` double NOT NULL,
+  `initial_price` double NOT NULL,
+  `current_price` double NOT NULL,
+  `buyer_email` varchar(100) NOT NULL,
+  `seller_email` varchar(100) NOT NULL,
+  PRIMARY KEY (`aID`),
+  KEY `buyer_email, seller_email_idx` (`buyer_email`),
+  KEY `seller_email_idx` (`seller_email`),
+  CONSTRAINT `buyer_email` FOREIGN KEY (`buyer_email`) REFERENCES `user` (`email`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `seller_email` FOREIGN KEY (`seller_email`) REFERENCES `user` (`email`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auction_posts`
+--
+
+LOCK TABLES `auction_posts` WRITE;
+/*!40000 ALTER TABLE `auction_posts` DISABLE KEYS */;
+INSERT INTO `auction_posts` VALUES (0,'08:00:00','2022-05-05 00:00:00',5,100,100,'user2@gmail.com','user1@gmail.com'),(1,'08:00:00','2022-05-05 00:00:36',10,20,30,'user2@gmail.com','user1@gmail.com');
+/*!40000 ALTER TABLE `auction_posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bid`
+--
+
+DROP TABLE IF EXISTS `bid`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bid` (
+  `email` varchar(100) NOT NULL,
+  `aID` int NOT NULL,
+  `bidID` int NOT NULL,
+  `max_bid` double DEFAULT NULL,
+  PRIMARY KEY (`email`,`aID`),
+  KEY `auction_ID_idx` (`aID`),
+  CONSTRAINT `auction_ID` FOREIGN KEY (`aID`) REFERENCES `auction_posts` (`aID`),
+  CONSTRAINT `email` FOREIGN KEY (`email`) REFERENCES `user` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bid`
+--
+
+LOCK TABLES `bid` WRITE;
+/*!40000 ALTER TABLE `bid` DISABLE KEYS */;
+INSERT INTO `bid` VALUES ('user2@gmail.com',0,0,200),('user2@gmail.com',1,1,100);
+/*!40000 ALTER TABLE `bid` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item` (
+  `itemID` int NOT NULL,
+  `aID` int NOT NULL,
+  `item_name` varchar(20) NOT NULL,
+  `item_type` enum('shirts','pants','shoes') NOT NULL,
+  PRIMARY KEY (`itemID`,`aID`),
+  KEY `aID_idx` (`aID`),
+  CONSTRAINT `aID` FOREIGN KEY (`aID`) REFERENCES `auction_posts` (`aID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+INSERT INTO `item` VALUES (0,0,'Levi Jeans','pants'),(1,1,'Rincon 2','shoes');
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `question`
+--
+
+DROP TABLE IF EXISTS `question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `question` (
+  `qID` int NOT NULL,
+  `question` varchar(200) NOT NULL,
+  `answer` varchar(200) DEFAULT NULL,
+  `user` varchar(20) NOT NULL,
+  PRIMARY KEY (`qID`),
+  KEY `question_poster_idx` (`user`),
+  CONSTRAINT `question_poster` FOREIGN KEY (`user`) REFERENCES `user` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `question`
+--
+
+LOCK TABLES `question` WRITE;
+/*!40000 ALTER TABLE `question` DISABLE KEYS */;
+INSERT INTO `question` VALUES (0,'When will my order deliver?','2-4 weeks','user1@gmail.com'),(1,'Can I make a return?',NULL,'user2@gmail.com');
+/*!40000 ALTER TABLE `question` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
@@ -40,7 +183,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('user1@gmail.com','user1','1234',0,1);
+INSERT INTO `user` VALUES ('user1@gmail.com','user1','1234',0,1),('user2@gmail.com','user2','1234',1,0);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +196,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-05 14:57:34
+-- Dump completed on 2022-05-02 22:16:28
